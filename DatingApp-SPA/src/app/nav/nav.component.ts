e7a3e96942bacb8a88856958b0e236b0b38d6082
Haitham-cmd <Haitham.abdelghani@gmail.com>
2019-06-21 05:05:12 +0200

    End of section 7

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/Alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,8 @@ import { AlertifyService } from '../_services/Alertify.service';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  constructor(public authService: AuthService, private alertifyService: AlertifyService) { }
+  constructor(public authService: AuthService, private alertifyService: AlertifyService,
+    private router: Router) { }
 
   ngOnInit() {
   }
@@ -21,6 +23,8 @@ export class NavComponent implements OnInit {
         this.alertifyService.success('Logged in successfully');
       }, error => {
         this.alertifyService.error('Faild to login');
+      }, () => {
+        this.router.navigate(['/members']);
       });
   }
 
@@ -32,6 +36,7 @@ export class NavComponent implements OnInit {
     if (!!localStorage.getItem('token')) {
       localStorage.removeItem('token');
       this.alertifyService.warning('logged out');
+      this.router.navigate(['/home']);
     }
   }
 
